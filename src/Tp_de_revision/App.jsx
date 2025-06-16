d9e726f6578bc419488d5e111c1e4f24497e01a3
Abdelghafour ikbaiss <ikbaissabdelghafour04@gmail.com>
2025-06-16 03:57:17 +0100

    Update App.jsx

--- src/Tp_de_revision/App.jsx
+++ src/Tp_de_revision/App.jsx
@@ -4,7 +4,7 @@ import Na3na3 from './Na3na3'
 function App() {
 
 const kissan={
-    object_kass:{id:1,nom:"wadia",prenom:"salmano"},
+    object_kass:{id:1,nom:"walid",prenom:"salim"},
     table_kass:["ikbaisson" , "abdo" , 20]
     }
 const brad="brad n atay seba3"
